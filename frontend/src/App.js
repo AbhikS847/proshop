48fce82f61db09ead49e4cdd4d82c0dae26b7670
AbhikS847 <abhikshrestha@gmail.com>
2021-03-28 22:52:59 +1100

    Front end finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import './index.css';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-    <h1>Welcome to ProShop</h1>
-    </>
+    <Router>
+    <Header />
+    <main className="py-3">
+    <Container>
+    <Route path="/" exact component = {HomeScreen}></Route>
+    <Route path="/product/:id" exact component = {ProductScreen}></Route>
+    </Container>
+    </main>
+    <Footer />
+    </Router>
   );
 }
 
