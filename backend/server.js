71841cfab51d33e13a93e9e181d594332da5438c
AbhikS847 <abhikshrestha@gmail.com>
2021-05-06 22:39:59 +1000

    Finished up the routing and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import {notFound,errorHandler} from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -10,16 +11,14 @@ connectDB();
 
 const app = express();
 
-app.get('/api/products',(req,res)=>{
-    res.json(products);
-})
-
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find(p=>p._id === req.params.id);
-    res.json(product);
-})
 
 const PORT = process.env.PORT || 5000;
+
+app.use('/api/products',productRoutes);
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 
 app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold));
