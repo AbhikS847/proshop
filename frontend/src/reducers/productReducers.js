3181e6bce6a5890089d319b01c28759814c9f435
AbhikS847 <abhikshrestha@gmail.com>
2021-05-18 22:02:34 +1000

    added redux to alot of stuff

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productConstants';
+import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL,PRODUCT_DETAILS_FAIL,PRODUCT_DETAILS_SUCCESS,PRODUCT_DETAILS_REQUEST} from '../constants/productConstants';
 export const productListReducer = (state={products:[]},action) =>{
     switch(action.type){
         case PRODUCT_LIST_REQUEST:
@@ -20,3 +20,26 @@ export const productListReducer = (state={products:[]},action) =>{
             return state
     }
 }
+
+export const productDetailsReducer = (state={product:{reviews:[]}},action) =>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                loading:true,
+                ...state
+            }
+        case PRODUCT_DETAILS_SUCCESS:
+            return{
+                loading:false,
+                product:action.payload
+            }
+        case PRODUCT_DETAILS_FAIL:
+            return{
+                loading:false,
+                error:action.payload
+            }
+        default:
+            return state
+    }
+}
+
