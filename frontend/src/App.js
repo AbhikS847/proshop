b2e3824252de4005c6110326d025e7f3d6907de7
AbhikS847 <abhikshrestha@gmail.com>
2021-05-24 22:39:37 +1000

    Added the cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import {Container} from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import './index.css';
 import ProductScreen from './screens/ProductScreen';
@@ -15,6 +16,7 @@ const App = () => {
     <Container>
     <Route path="/" exact component = {HomeScreen}></Route>
     <Route path="/product/:id" exact component = {ProductScreen}></Route>
+    <Route path="/cart/:id?" exact component = {CartScreen}></Route>
     </Container>
     </main>
     <Footer />
